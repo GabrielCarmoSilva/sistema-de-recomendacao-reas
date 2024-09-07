c38fd8bcbc26cdfeccbaf054082bcb2dd6d2916f
Gabriel Silva <gabriel.ccarmo@gmail.com>
2024-09-07 03:41:20 -0300

    gitignore

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,14 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Console\Scheduling\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+Schedule::call(function () {
+    if (!Str::contains(Shell::exec('ps aux | grep "queue:work"'), 'artisan queue:work')) {
+        Artisan::call('queue:work');
+    }
+})->everyTenSeconds();
